2e12f065ef7f42f224922432cc74073ded9dc9a6
euvi <vitoria.t.salomao@gmail.com>
2024-10-05 20:24:43 -0300

    acrescentando o contador de tentativas

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 let chute;
+let tentativas = 1;
 //comparando o chute com o numeroSecreto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 30');
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
         if (chute > numeroSecreto) {
             alert('O número secreto é menor que ' + chute);
         } else {
             alert('O número secreto é maior que ' + chute);
         }
+        tentativas ++;
     }
 }
