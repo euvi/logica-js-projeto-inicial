c10dbca7c341ea1101c384b8eebddb0613546761
euvi <vitoria.t.salomao@gmail.com>
2024-10-05 19:46:59 -0300

    acrescentando mensagem do erro

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
+let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 30');
 
+//comparando o chute com o numeroSecreto
 if(chute == numeroSecreto){
-    console.log('Isso aí! Você descobriu o número secreto (5)')
-};
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+} else{
+    console.log('Você errou :( ');
+}
