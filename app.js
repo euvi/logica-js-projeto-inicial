7ebe75cd1c0942dbf23285aa2e010a133f48ae60
euvi <vitoria.t.salomao@gmail.com>
2024-10-05 20:20:56 -0300

    acrescentando o loop

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 30');
-
+let chute;
 //comparando o chute com o numeroSecreto
-if(chute == numeroSecreto){
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else{
-   alert('Você errou :( ');
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert('O número secreto é menor que ' + chute);
+        } else {
+            alert('O número secreto é maior que ' + chute);
+        }
+    }
 }
